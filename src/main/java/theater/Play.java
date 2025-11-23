605042270c4cd78324f4f9db65086c86d455b5a7
shiraz42 <shirazlarik42@gmail.com>
2025-11-23 18:53:49 -0500

    Task 1.1: Fixed all CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,48 @@
 package theater;
 
+/**
+ * Represents a play with a name and type (e.g., tragedy, comedy).
+ * Both fields are non-null strings.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
+    /**
+     * Construct a Play with the given name and type.
+     *
+     * @param name the name of the play (non-null)
+     * @param type the type of the play (non-null)
+     */
     public Play(String name, String type) {
+        this.setName(name);
+        this.setType(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the name of the play.
+     *
+     * @param name the name to set (non-null)
+     */
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Set the type of the play.
+     *
+     * @param type the type to set (non-null)
+     */
+    public void setType(String type) {
         this.type = type;
     }
 }
---
+++ src/test/java/theater/PlayTest.java
@@ -0,0 +1,7 @@
+package theater;
+
+import junit.framework.TestCase;
+
+public class PlayTest extends TestCase {
+
+}
